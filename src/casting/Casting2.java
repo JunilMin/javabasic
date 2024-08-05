f0b1a1fa27122e41b525baa5b7a837968f6ee2b8
JUNILMIN <jimin@aim.co.kr>
2024-08-05 13:31:43 +0900

    형 변환

--- src/casting/Casting2.java
+++ src/casting/Casting2.java
@@ -2,22 +2,28 @@ package casting;
 
 public class Casting2 {
 
-    public static void main(String[] args) {
-        double doubleValue = 1.5;
-        int intValue = 0;
-        String a;
+  public static void main(String[] args) {
+    double doubleValue = 1.5;
+    int intValue = 0;
+    String a;
 
-        double doubleValue2 = 1;
-        int intValue2;
+    double doubleValue2 = 1;
+    int intValue2;
 //        intValue2 = doubleValue2;  // 컴파일 오류 발생
-        //intValue = doubleValue; //컴파일 오류 발생
-        intValue = (int) doubleValue; ///형변환
-        System.out.println(intValue);
+    //intValue = doubleValue; //컴파일 오류 발생
+    intValue = (int) doubleValue; ///형변환
+    System.out.println(intValue);
 
+    float floatN = 1;
 
-        int intValue3 = 0;
-        long longValue = 0;
-        longValue = (long) doubleValue;
-        System.out.println(longValue);
-    }
+    intValue = (int) floatN;
+    intValue = 2;
+    System.out.println(intValue);
+
+    int intValue3 = 0;
+    long longValue = 0;
+    longValue = (long) doubleValue;
+    System.out.println(longValue);
+
+  }
 }
