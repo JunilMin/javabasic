cfeac0417a80f8c3622e7509396313d08284ef86
JUNILMIN <jimin@aim.co.kr>
2024-08-05 13:27:53 +0900

    형 변환

--- src/casting/Casting4.java
+++ src/casting/Casting4.java
@@ -2,28 +2,27 @@ package casting;
 
 public class Casting4 {
 
-    public static void main(String[] args) {
-        int div1 = 3 / 2;
-        System.out.println("div1 = " + div1); //1
+  public static void main(String[] args) {
+    int div1 = 3 / 2;
+    System.out.println("div1 = " + div1); //1
 
-        double div2 = 3 / 2;
-        System.out.println("div2 = " + div2); //1.0
+    double div2 = 3 / 2;
+    System.out.println("div2 = " + div2); //1.0
 
-        double div3 = 3.0 / 2.0;
-        System.out.println("div3 = " + div3); //1.5
+    double div3 = 3.0 / 2.0;
+    System.out.println("div3 = " + div3); //1.5
 
-        double div4 = (double) 3 / 2;
-        System.out.println("div4 = " + div4); //1.5
+    double div4 = (double) 3 / 2;
+    System.out.println("div4 = " + div4); //1.5
 
-        int a = 3;
-        int b = 2;
-        double result = (double) a / b;
-        System.out.println("result = " + result); //1.5
+    int a = 3;
+    int b = 2;
+    double result = (double) a / b;
+    System.out.println("result = " + result); //1.5
 
-        int intN = 5;
-        double doubleN = 10;
-        System.out.println(intN+doubleN); // 다른 타입의 계산은 큰 범위의 타입으로 자동 형 변환됨
+    int intN = 5;
+    double doubleN = 10;
+    System.out.println(intN + doubleN); // 다른 타입의 계산은 큰 범위의 타입으로 자동 형 변환됨
 
-
-    }
+  }
 }
